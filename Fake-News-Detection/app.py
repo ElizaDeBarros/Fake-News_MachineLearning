8d7bfee85e9c881f2cd49ff5bde111d66868a734
DerekLarson77 <DerekLarson77@gmail.com>
2021-08-24 15:45:36 -0700

    Restructured folder layout

--- Fake-News-Detection/app.py
+++ Fake-News-Detection/app.py
@@ -1,7 +1,7 @@
 import psycopg2
+import os
 import sys
-from  flask import Flask,render_template
-from flask import jsonify
+from  flask import Flask, render_template, jsonify, request, redirect
 
 
 app = Flask(__name__)
@@ -27,4 +27,4 @@ def send_data():
     return jsonify(data)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=True)
