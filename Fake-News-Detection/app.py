07699ac374edf6fb0786fad747fcde3f0bec3718
DerekLarson77 <DerekLarson77@gmail.com>
2021-08-24 21:18:09 -0700

    app.py works locally now

--- Fake-News-Detection/app.py
+++ Fake-News-Detection/app.py
@@ -12,41 +12,55 @@ app = Flask(__name__)
 
 from config import username, password, server, database
 
-conn = f"host='{server}' dbname='{database}' user='{username}' password='{password}'"
+conn = f'postgresql+psycopg2://{username}:{password}@{server}/{database}'
 
-con = psycopg2.connect(conn)  
-cur = con.cursor()
-cur.execute("""select * from  training_data""")
-data = [col for col in cur]
+from flask_sqlalchemy import SQLAlchemy
+#app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', '') or "sqlite:///db.sqlite"
+app.config['SQLALCHEMY_DATABASE_URI'] = conn
+# Remove tracking modifications
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
-training_list = []
+db = SQLAlchemy(app)
 
-for result in data:
+class Training(db.Model):
+    __tablename__ = 'training_data'
 
-    data_training = {
-        "index": result[0],
-        "URLs": result[1],
-        "Headline": result[2],
-        "Body": result[3],
-        "Label": result[4]
-    }
-    training_list.append(data_training)
+    index = db.Column(db.BigInteger, primary_key=True)
+    urls = db.Column(db.Text)
+    headline = db.Column(db.Text)
+    body = db.Column(db.Text)
+    label = db.Column(db.BigInteger)
+
+    def __repr__(self):
+        return '<Training %r>' % (self.urls)
 
 #################################################
 # Flask Routes
 #################################################
 
+# create route that renders index.html template
 @app.route("/")
 def home():
-    return(
-        #"/data will take you to the json information"
-        render_template("index.html")
-    )
+    return render_template("index.html")
 
-@app.route('/data/api')
+@app.route("/data/api")
 def return_json():
+    results = db.session.query(Training.index, Training.urls, Training.headline, Training.body, Training.label).all()
+
+    training_list = []
+
+    for result in results:
+
+        data_training = {
+            "index": result[0],
+            "URLs": result[1],
+            "Headline": result[2],
+            "Body": result[3],
+            "Label": result[4]
+        }
+        training_list.append(data_training)
 
     return jsonify(training_list)
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+if __name__ == "__main__":
+    app.run()
