cd76084b29b6813b2c1937efdb115365bb5ed60d
DerekLarson77 <DerekLarson77@gmail.com>
2021-09-01 12:08:33 -0700

    Update app.js

--- Fake-News-Detection/static/js/app.js
+++ Fake-News-Detection/static/js/app.js
@@ -1,25 +1,64 @@
+const url = "/data/predictions";
+
 function init() {
     // Use D3 to select the dropdown menu
     var dropdownMenu = d3.select("#selDataset");
-    // 1. Use the D3 library to read in `samples.json`.
-    const url = "/data/predictions";
 
-d3.json(url).then(function(data) {
-        dataNews = data.Body;   
-        bodyList = [];
-        for (var i=0; i < data.length; i++) {
-            bodyList.push(data[i].index)
-        };
-        
+    d3.json(url).then(function(data) {
+
+        let uniqueHeadline = [...new Set(data.map(item => item.Headline))];
+  
+        //bodyList = [];
+        //for (var i=0; i < data.length; i++) {
+        //    bodyList.push(data[i].index)
+        //};
+            
         // add the options to the button
         dropdownMenu.selectAll("option")
-            .data(bodyList)
+            .data(uniqueHeadline)
             .enter()
             .append("option")
             .text(a => a)
             .attr('value', a => a);
 
+        answers(uniqueHeadline[2])
+    });
+
+
+};
+
+// On change to the dropdown menu
+d3.selectAll("#selDataset").on("change", optionChanged);
+
+// Function called by DOM changes
+function optionChanged() {
+    var dropdownMenu = d3.select("#selDataset");
+    // Assign the value of the dropdown menu option to a variable
+    var newHeadline = dropdownMenu.property("value");
+    // Initialize an empty array for the country's data
+    answers(newHeadline);
+};
+
+function answers(headline) {  
+    d3.json(url).then(function(data) {
+        console.log(headline)
+
+        answer_dict = {}
+        for (var i=0; i < data.length; i++) {
+            //console.log(data[i].index)
+            if (data[i].index === headline) {                
+                answer_dict = {"Headline": data[i].Headline,
+                 "Logistic_Regression": data[i].Logistic_Regression,
+                 "Naive_Bayes": data[i].Naive_Bayes,
+                 "Decision_Tree": data[i].Decision_Tree,
+                 "Passive_Aggressive_Classifier": data[i].Passive_Aggressive_Classifier
+                }
+            }
+        };
+        console.log(answer_dict)
     });
 };
+
+
 
 init();
